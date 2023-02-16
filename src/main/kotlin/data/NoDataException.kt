e5f663b4aa2159518351e1c837ed969721064b2e
Di.Est <di.est.52@gmail.com>
2023-02-17 00:48:56 +0300

    small fix

--- src/main/kotlin/data/NoDataException.kt
+++ src/main/kotlin/data/NoDataException.kt
@@ -4,4 +4,4 @@ package data
  * @author Demitrist on 16.02.2023
  **/
 
-class NoDataException(message:String) : Exception(message = message)
+class NoDataException(message:String) : Exception(message)
