bd3f4daffd2aadd1fa677b7a2824f8e504a3b257
Di.Est <di.est.52@gmail.com>
2023-02-16 23:00:11 +0300

    repository implementation

--- src/main/kotlin/data/BaseNewsRepository.kt
+++ src/main/kotlin/data/BaseNewsRepository.kt
@@ -1,15 +1,29 @@
 package data
 
+import data.models.ResultData
 import domain.NewsRepository
+import domain.models.ResultDomain
 
 /**
  * @author Demitrist on 13.02.2023
  **/
 
 class BaseNewsRepository(
-    private val dataSource: DataSource,
+    private val cloud: DataSource,
+    private val cache: DataSource.Mutable,
     private val mapper: ResultDataToDomainMapper
 ) : NewsRepository {
 
-    override suspend fun fetchNews() = dataSource.fetchData().map(mapper)
+    override suspend fun fetchNews(): ResultDomain {
+        val result: ResultData
+        if (cache.haveData()){
+            result = cache.fetchData()
+        }
+        else
+        {
+            result = cloud.fetchData()
+            cache.save(result)
+        }
+        return result.map(mapper = mapper)
+    }
 }
