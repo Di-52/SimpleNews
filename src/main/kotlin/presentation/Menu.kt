9cae20ea0bbbc24f485a47e73f8995c8377ba550
Di.Est <di.est.52@gmail.com>
2023-02-19 00:22:11 +0300

    menu

--- src/main/kotlin/presentation/Menu.kt
+++ src/main/kotlin/presentation/Menu.kt
@@ -1,27 +1,31 @@
 package presentation
 
 /**
- * @author Demitrist on 11.02.2023
+ * @author Demitrist on 15.02.2023
  **/
 
-class Menu() {
+interface Menu {
 
-    fun showMenu(level:Int = 1){
-        when(level){
-            1->println("Type one digit")
-            2->{
-                println("1. Show all news")
-                println("2. Show sorted news (Date asc)")
-                println("3. Show sorted news (Date desc)")
-                println("3. Filter by keyword")
+    fun show()
+    fun showForResult()
+
+    class Base(
+        private val name: String,
+        private val items: List<MenuItem>,
+        private val userInputReader: UserInput
+    ) : Menu {
+
+        override fun show() {
+            println(name)
+            items.forEach { menuItem ->
+                menuItem.show()
             }
         }
-    }
-    fun showKeywords(keys:List<String>){
-        println("Select word for filter: ")
-        keys.forEachIndexed{index,key ->
-            var i = index+1
-            println("$i. $key")
+
+        override fun showForResult() {
+            show()
+            val result = userInputReader.wait(valFrom = 1, items.size)
+            items[result-1].action()
         }
     }
 }
